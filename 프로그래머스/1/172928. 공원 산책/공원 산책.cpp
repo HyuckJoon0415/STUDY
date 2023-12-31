e4324a8e7fa317f89e02740965b59346e7cda935
HyuckJoon0415 <145080176+HyuckJoon0415@users.noreply.github.com>
2023-12-31 20:45:23 +0900

    [level 1] Title: 공원 산책, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
@@ -1,94 +1,41 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <sstream>
 
-using namespace  std;
-
-struct Point {
-	int row, col;
-};
-
-int D[4][2] = {
-	{-1,0}, //North
-	{1,0},  //South
-	{0,-1},	//West
-	{0,1}	//East
-};
+using namespace std;
 
 vector<int> solution(vector<string> park, vector<string> routes) {
-	int parkRowSize = 0;
-	int parkColSize = 0;
-	parkRowSize = park.size()-1; //park row length
-	parkColSize = park[0].length()-1; //park col length	
-
-	int sr = 0, sc = 0; //Start Node coordinates	
-	Point cur{0,0}; //Current Node
-
-	bool endFlag = false;
-	for (int i = 0; i < parkRowSize+1; i++) {
-		if (endFlag) break;
-		for (int j = 0; j < parkColSize+1; j++) {
-			if (park[i][j] == 'S') {
-				cur.row = i;	//Get Start Node Coordinates
-				cur.col = j;					
-				endFlag = true;
-				break;
-			}
+	vector<int> answer;
+	int row = park.size() - 1;
+	int col = park[0].size() - 1;
+	int sr = 0, sc = 0;
+	for (int i = 0; i < park.size(); i++) {
+		for (int j = 0; j < park[i].size(); j++) {
+			if (park[i][j] == 'S') { sr = i; sc = j; }
 		}
-	}	
-
-	int routesRowSize = routes.size();	
-	for (int i = 0; i < routesRowSize; i++) {
-		istringstream  iss(routes[i]); //routes[i] parsing
-		char op;	//Direction
-		int n=0;	//Num
-		iss >> op >> n;		
-
-		int cr = 0, cc = 0; //Current Coordinates
-		cr = cur.row;
-		cc = cur.col;
-
-		if (op == 'E') { //East
-			for (int j = 0; j < n; j++) {
-				cc += D[3][1];
-				if (cc > parkColSize || park[cr][cc] == 'X') {
-					cc = cur.col;
-					break;
-				}
-			}			
-		}
-		else if (op=='W') { //West
-			for (int j = 0; j < n; j++) {
-				cc += D[2][1];
-				if (cc < 0 || park[cr][cc] == 'X') {
-					cc = cur.col;
-					break;
-				}
-			}			
-		}
-		else if (op == 'N') { //North
-			for (int j = 0; j < n; j++) {
-				cr += D[0][0];
-				if (cr < 0 || park[cr][cc] == 'X') {
-					cr = cur.row;
-					break;
-				}
-			}			
-		}
-		else { //South
-			for (int j = 0; j < n; j++) {
-				cr += D[1][0];
-				if (cr > parkRowSize || park[cr][cc] == 'X') {
-					cr = cur.row;
-					break;
-				}
-			}			
-		}
-		cur.row = cr;
-		cur.col = cc;
 	}
 
-	vector<int> answer={cur.row, cur.col};
-	return answer;	
+	for (int i = 0; i < routes.size(); i++) {
+		int Idx = routes[i][2] - 48;
+		for (int j = 0; j < Idx; j++) {
+			if (routes[i][0] == 'E') {
+				sc++;
+				if (park[sr][sc] == 'X' || sc > col) { sc -= (j + 1);  break; }
+			}
+			else if (routes[i][0] == 'W') {
+				sc--;
+				if (park[sr][sc] == 'X' || sc < 0) { sc += (j + 1); break; }
+			}
+			else if (routes[i][0] == 'S') {
+				sr++;
+				if (park[sr][sc] == 'X' || sr > row) { sr -= (j + 1); break; }
+			}
+			else if (routes[i][0] == 'N') {
+				sr--;
+				if (park[sr][sc] == 'X' || sr < 0) { sr += (j + 1); break; }
+			}
+		}
+	}
+	answer.emplace_back(sr);
+	answer.emplace_back(sc);
+	return answer;
 }
