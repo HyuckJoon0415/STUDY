4f98b48a49740109d4cfc8aafb1d6755ded91ee8
HyuckJoon0415 <145080176+HyuckJoon0415@users.noreply.github.com>
2023-12-31 10:28:42 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,22 +1,20 @@
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
 
 string solution(vector<int> food) {
-	string leftStart;	
-	string revLeftStart;
-
-	for (int i = 1; i < food.size(); i++) {
-		if (food[i]<2) continue;	//Check even		
-		for (int j = 0; j < food[i] / 2; j++) {
-			leftStart += to_string(i);
-		}
-	}
-	revLeftStart = leftStart;
-	reverse(revLeftStart.begin(), revLeftStart.end());
-	leftStart += "0";
-	leftStart += revLeftStart;
-	return leftStart;
+    string answer = "";
+    string temp = "";
+    for (int i = 1; i < food.size();i++) {
+        for (int j = 0; j < food[i] / 2; j++) {
+            answer += to_string(i);
+            temp = answer;
+        }
+    }
+    reverse(temp.begin(), temp.end());
+    answer += "0";
+    answer += temp;
+    return answer;
 }
